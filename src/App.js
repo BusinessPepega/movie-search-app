879c8c717b5d7d433189908d965bbb8501cebc35
Taltis <alexborcooperation@gmail.com>
2022-06-14 11:45:19 -0600

    a note no one will read

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import SearchMovies from './components/SearchMovie';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">React Movie Search</h1>
+      <SearchMovies/>
+    </div>
+  );
+}
+
+export default App;
